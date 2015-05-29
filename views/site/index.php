d13d4931ffa93fa225ecd9f2361a9f8ad62ca0c9
thuc <fitness@hocmai.com.vn>
2015-05-29 13:38:00 +0700

    v1.0.0.1

--- views/site/index.php
+++ views/site/index.php
@@ -1,425 +1,68 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-<head>
 
-<!--------------------
-LOGIN FORM
-by: Amit Jakhu
-www.amitjakhu.com
---------------------->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->baseUrl; ?>/media/css/bootstrap/bootstrap.min.css" />
 
-<!--META-->
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Speak up | Online english tutoring</title>
+    <!-- Optional theme -->
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->baseUrl; ?>/media/css/bootstrap/bootstrap-theme.min.css" />
 
-<!--STYLESHEETS-->
-
-<!--SCRIPTS-->
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.2.6/jquery.min.js"></script>
-<!--Slider-in icons-->
-<script type="text/javascript">
-$(document).ready(function() {
-	$(".username").focus(function() {
-		$(".user-icon").css("left","-48px");
-	});
-	$(".username").blur(function() {
-		$(".user-icon").css("left","0px");
-	});
-	
-	$(".password").focus(function() {
-		$(".pass-icon").css("left","-48px");
-	});
-	$(".password").blur(function() {
-		$(".pass-icon").css("left","0px");
-	});
-});
-</script>
-
-</head>
-<body>
-
-<!--WRAPPER-->
-<div id="wrapper">
-
-	<!--SLIDE-IN ICONS-->
-    <div class="user-icon"></div>
-    <div class="pass-icon"></div>
-    <!--END SLIDE-IN ICONS-->
-
-<!--LOGIN FORM-->
-<form name="login-form" class="login-form" action="<?php echo Yii::app()->baseurl;?>/site/signin" method="post" >
-	<!--HEADER-->
-    <div class="header">
-    <!--TITLE--><h1>Speak up</h1><!--END TITLE-->
-    <span class="text-warning text-center">
-        <?php 
-        foreach(Yii::app()->user->getFlashes() as $key => $message) : 
-            echo  $message; 
-        endforeach;
-        ?>
-    </span>
-    <?php 
-	$tenmien="http";
-	if(isset($_SERVER['HTTPS'])){
-            $tenmien.="s";	
-	}
-	$tenmien.="://".$_SERVER['SERVER_NAME'];
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->baseUrl; ?>/media/css/bootstrap/login.css" />
+  
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <?php
+        $tenmien="http";
+        if(isset($_SERVER['HTTPS'])){
+            $tenmien.="s";
+        }
+        $tenmien.="://".$_SERVER['SERVER_NAME'];
     ?>
-    <!--DESCRIPTION--><a href=<?php echo $tenmien."/news/dang-ky"?>><br> Don't have an account? Sign up now!</a><!--END DESCRIPTION-->
-    <!--DESCRIPTION--><a href=<?php echo $tenmien."/news/lien-he"?>><br> Need help? Contact us!</a><!--END DESCRIPTION-->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3 col-md-4 ">
+                <img class="trai-img" src="<?php echo Yii::app()->baseUrl; ?>/media/images/trai.png" alt="">
+            </div>
+            <div class="col-sm-6 col-md-4 ">
+                <div class="account-wall">
+                     <img class="profile-img" src="<?php echo Yii::app()->baseUrl; ?>/media/images/logo-speakup.png"
+                    alt="">
+                    <form name="login-form" class="form-signin" action="<?php echo Yii::app()->baseurl;?>/site/signin" method="post">
+                        
+                        <input name="email" type="text" class="form-control" placeholder="Username ..." required autofocus>
+                        <input name="password" type="password" class="form-control" placeholder="Password ..." required>
+                        <span class="text-center test-error">
+                            <?php 
+                            foreach(Yii::app()->user->getFlashes() as $key => $message) : 
+                                echo  $message; 
+                            endforeach;
+                            ?>
+                        </span>
+                        <a href="<?php echo $tenmien."/news/dang-ky" ?>" class="pull-left new-account">Create an account? </a> 
+                        <a href='<?php echo $tenmien."/news/lien-he"?>' class="pull-right need-help">Need help? </a>
+                        <span class="clearfix"></span>
+                        <button class="btn btn-lg btn-warning btn-block btn-login" type="submit">
+                            Sign in</button>
+                    </form>
+                </div>
+                
+            </div>
+            <div class="col-sm-3 col-md-4 ">
+                <img class="phai-img" src="<?php echo Yii::app()->baseUrl; ?>/media/images/phai.png" alt="">
+            </div>
+        </div>
     </div>
-    <!--END HEADER-->
-	
-	<!--CONTENT-->
-    <div class="content">
-	<!--USERNAME--><input name="username" type="text" class="input username"  placeholder="Username.."  /><!--END USERNAME-->
-    <!--PASSWORD--><input name="password" type="password" class="input password" placeholder="Password.."  /><!--END PASSWORD-->
-    </div>
-    <!--END CONTENT-->
-    
-    <!--FOOTER-->
-    <div class="footer">
-    <!--LOGIN BUTTON--><input type="submit" name="submit" value="Login" class="button" /><!--END LOGIN BUTTON-->
-    <!--REGISTER BUTTON--> <a href=<?php echo $tenmien ?> class="register" > Back to homepage </a>
-    
-    <!--END REGISTER BUTTON-->
-    </div>
-    <!--END FOOTER-->
 
-</form>
-<!--END LOGIN FORM-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="<?php echo Yii::app()->baseUrl; ?>/media/js/bootstrap/jquery.min.js"></script>
+      
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="<?php echo Yii::app()->baseUrl; ?>/media/js/bootstrap/bootstrap.min.js"></script>
 
-</div>
-<!--END WRAPPER-->
-
-<!--GRADIENT--><div class="gradient"></div><!--END GRADIENT-->
-
-</body>
+  </body>
 </html>
-<style>
-    /*******************
-LOGIN FORM STYLESHEET
-by: Amit Jakhu
-www.amitjakhu.com
-*******************/
-
-/*******************
-FONTS
-*******************/
-
-@import url(https://fonts.googleapis.com/css?family=Bree+Serif);
-
-/*******************
-SELECTION STYLING
-*******************/
-
-::selection {
-	color: #fff;
-	background: #f676b2; /* Safari */
-}
-::-moz-selection {
-	color: #fff;
-	background: #f676b2; /* Firefox */
-}
-
-/*******************
-BODY STYLING
-*******************/
-
-* {
-	margin: 0;
-	padding: 0;
-	border: 0;
-}
-
-body {
-	background: url(../themes/daykem/images/login/bg.png) repeat;
-	font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
-	font-weight:300;
-	text-align: left;
-	text-decoration: none;
-}
-
-#wrapper {
-	/* Center wrapper perfectly */
-	width: 300px;
-	height: 400px;
-	position: absolute;
-	left: 50%;
-	top: 50%;
-	margin-left: -150px;
-	margin-top: -200px;
-}
-
-/*
-.gradient {
-	width: 600px;
-	height: 600px;
-	position: fixed;
-	left: 50%;
-	top: 50%;
-	margin-left: -300px;
-	margin-top: -300px;
-	
-	background: url(../images/gradient.png) no-repeat;
-}
-*/
-a{
-    color: #3f9db8;
-    text-decoration: none;
-    font-size: 11px;
-    line-height: 16px;
-    color: #678889;
-    text-shadow: 1px 1px 0 rgba(256,256,256,1.0);
-    height: 21px;
-}
-.gradient {
-	/* Center Positioning */
-	width: 600px;
-	height: 600px;
-	position: fixed;
-	left: 50%;
-	top: 50%;
-	margin-left: -300px;
-	margin-top: -300px;
-	
-	/* Fallback */ 
-	background-image: url(../themes/daykem/images/login/gradient.png); 
-	background-repeat: no-repeat; 
-	
-	/* CSS3 Gradient */
-	background-image: -webkit-gradient(radial, 0% 0%, 0% 100%, from(rgba(213,246,255,1)), to(rgba(213,246,255,0)));
-	background-image: -webkit-radial-gradient(50% 50%, 40% 40%, rgba(213,246,255,1), rgba(213,246,255,0));
-	background-image: -moz-radial-gradient(50% 50%, 50% 50%, rgba(213,246,255,1), rgba(213,246,255,0));
-	background-image: -ms-radial-gradient(50% 50%, 50% 50%, rgba(213,246,255,1), rgba(213,246,255,0));
-	background-image: -o-radial-gradient(50% 50%, 50% 50%, rgba(213,246,255,1), rgba(213,246,255,0));
-}
-
-/*******************
-LOGIN FORM
-*******************/
-
-.login-form {
-	width: 300px;
-	margin: 0 auto;
-	position: relative;
-	z-index:5;
-	
-	background: #f3f3f3;
-	border: 1px solid #fff;
-	border-radius: 5px;
-	
-	box-shadow: 0 1px 3px rgba(0,0,0,0.5);
-	-moz-box-shadow: 0 1px 3px rgba(0,0,0,0.5);
-	-webkit-box-shadow: 0 1px 3px rgba(0,0,0,0.5);
-}
-
-/*******************
-HEADER
-*******************/
-
-.login-form .header {
-	padding: 20px 30px 30px 30px;
-}
-
-.login-form .header h1 {
-	font-family: 'Bree Serif', serif;
-	font-weight: 300;
-	font-size: 28px;
-	line-height:34px;
-	color: #414848;
-	text-shadow: 1px 1px 0 rgba(256,256,256,1.0);
-	margin-bottom: 10px;
-}
-
-.login-form .header span {
-	font-size: 12px;
-	line-height: 16px;
-	color: red;
-	text-shadow: 1px 1px 0 rgba(256,256,256,1.0);
-}
-
-/*******************
-CONTENT
-*******************/
-
-.login-form .content {
-	padding: 0 30px 25px 30px;
-}
-
-/* Input field */
-.login-form .content .input {
-	width: 188px;
-	padding: 15px 25px;
-	
-	font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
-	font-weight: 400;
-	font-size: 14px;
-	color: #9d9e9e;
-	text-shadow: 1px 1px 0 rgba(256,256,256,1.0);
-	
-	background: #fff;
-	border: 1px solid #fff;
-	border-radius: 5px;
-	
-	box-shadow: inset 0 1px 3px rgba(0,0,0,0.50);
-	-moz-box-shadow: inset 0 1px 3px rgba(0,0,0,0.50);
-	-webkit-box-shadow: inset 0 1px 3px rgba(0,0,0,0.50);
-}
-
-/* Second input field */
-.login-form .content .password, .login-form .content .pass-icon {
-	margin-top: 25px;
-}
-
-.login-form .content .input:hover {
-	background: #dfe9ec;
-	color: #414848;
-}
-
-.login-form .content .input:focus {
-	background: #dfe9ec;
-	color: #414848;
-	
-	box-shadow: inset 0 1px 2px rgba(0,0,0,0.25);
-	-moz-box-shadow: inset 0 1px 2px rgba(0,0,0,0.25);
-	-webkit-box-shadow: inset 0 1px 2px rgba(0,0,0,0.25);
-}
-
-.user-icon, .pass-icon {
-	width: 46px;
-	height: 47px;
-	display: block;
-	position: absolute;
-	left: 0px;
-	padding-right: 2px;
-	z-index: 3;
-	
-	-moz-border-radius-topleft: 5px;
-	-moz-border-radius-bottomleft: 5px;
-	-webkit-border-top-left-radius: 5px;
-	-webkit-border-bottom-left-radius: 5px;
-}
-
-.user-icon {
-	top:147px; /* Positioning fix for slide-in, got lazy to think up of simpler method. */
-	background: rgba(65,72,72,0.75) url(../themes/daykem/images/login/user-icon.png) no-repeat center;	
-}
-
-.pass-icon {
-	top:221px;
-	background: rgba(65,72,72,0.75) url(../themes/daykem/images/login/pass-icon.png) no-repeat center;
-}
-
-/* Animation */
-.input, .user-icon, .pass-icon, .button, .register {
-	transition: all 0.5s;
-	-moz-transition: all 0.5s;
-	-webkit-transition: all 0.5s;
-	-o-transition: all 0.5s;
-	-ms-transition: all 0.5s;
-}
-
-/*******************
-FOOTER
-*******************/
-
-.login-form .footer {
-	padding: 25px 30px 40px 30px;
-	overflow: auto;
-	
-	background: #d4dedf;
-	border-top: 1px solid #fff;
-	
-	box-shadow: inset 0 1px 0 rgba(0,0,0,0.15);
-	-moz-box-shadow: inset 0 1px 0 rgba(0,0,0,0.15);
-	-webkit-box-shadow: inset 0 1px 0 rgba(0,0,0,0.15);
-}
-
-/* Login button */
-.login-form .footer .button {
-	float:right;
-	padding: 11px 25px;
-	
-	font-family: 'Bree Serif', serif;
-	font-size: 13px;
-	color: #fff;
-	text-shadow: 0px 1px 0 rgba(0,0,0,0.25);
-	
-	background: #56c2e1;
-	border: 1px solid #46b3d3;
-	border-radius: 5px;
-	cursor: pointer;
-	
-	box-shadow: inset 0 0 2px rgba(256,256,256,0.75);
-	-moz-box-shadow: inset 0 0 2px rgba(256,256,256,0.75);
-	-webkit-box-shadow: inset 0 0 2px rgba(256,256,256,0.75);
-}
-
-.login-form .footer .button:hover {
-	background: #3f9db8;
-	border: 1px solid rgba(256,256,256,0.75);
-	
-	box-shadow: inset 0 1px 3px rgba(0,0,0,0.5);
-	-moz-box-shadow: inset 0 1px 3px rgba(0,0,0,0.5);
-	-webkit-box-shadow: inset 0 1px 3px rgba(0,0,0,0.5);
-}
-
-.login-form .footer .button:focus {
-	position: relative;
-	bottom: -1px;
-	
-	background: #56c2e1;
-	
-	box-shadow: inset 0 1px 6px rgba(256,256,256,0.75);
-	-moz-box-shadow: inset 0 1px 6px rgba(256,256,256,0.75);
-	-webkit-box-shadow: inset 0 1px 6px rgba(256,256,256,0.75);
-}
-
-/* Register button */
-.login-form .footer .register {
-	display: block;
-	float: right;
-	padding: 10px;
-	margin-right: 25px;
-	
-	//background: none;
-	//border: none;
-        
-        border: 1px solid #3079ed;
-	border-radius: 5px;
-        
-	cursor: pointer;
-	
-	font-family: 'Bree Serif', serif;
-	font-size: 12px;
-	color: #fff;
-	//text-shadow: 0px 1px 0 rgba(256,256,256,0.5);
-        text-shadow: 0 1px rgba(0,0,0,0.1);
-        background: #4d90fe;
-}
-
-.login-form .footer .register:hover {
-	color: #3f9db8;
-        background: none;
-}
-
-.login-form .footer .register:focus {
-	position: relative;
-	bottom: -1px;
-}
-::selection {
-background: #56c2e1;
-color:#555;
-}
- 
-::-moz-selection {
-background:#f094b7;
-color:#555;
-}
- 
-::-webkit-selection {
-background:#f094b7;
-color:#555;
-}
-</style>
