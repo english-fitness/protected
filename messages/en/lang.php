fa64df7e1ff3eac9cdb004e3baf424d37c4ed5ad
thuc <fitness@hocmai.com.vn>
2015-06-11 14:33:53 +0700

    da ngon ngu cho man hinh cua hoc sinh

---
+++ messages/en/lang.php
@@ -0,0 +1,114 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+return array(
+    'Lịch học'=>'Schedule',
+    'Thay đổi ngôn ngữ'=>'Change Language',
+    'Loại lớp học'=>'Type of class',
+    "Tên lớp học"=>'Class name',
+    'Học sinh'=>'Attendees',
+    'Giáo viên'=>'Tutor',
+    'Ngày học'=>'Date',
+    'Giờ học'=>'Time',
+    'Vào lớp'=>'Enter the class',
+    'Không có buổi học nào đang diễn ra hoặc sắp tới trong vòng 1 tuần!'=>
+    'No session in process or coming within 1 week!',
+    'Lưu ý'=>'Note',
+    'Kiểm tra audio trước khi vào học để tránh ảnh hưởng đến buổi học'=>
+    'Please check audio befor entering the class',
+    'Click để kiểm tra ngay'=>'Check',
+    'Bắt đầu'=>'Start',
+    'Nghe lại'=>'Again',
+    'Buổi học gần nhất'=>'On-going sesions',
+    'Buổi học đã hoàn thành'=>'Complated sessions',
+    'Buổi học đã hoàn thành'=>'Complated sessions',
+    
+    'Thời gian học'=>'Time',
+    'Kiểm tra loa, mic'=>'Test speaker, microphone',
+    'Thông tin cá nhân'=>'Profile',
+    'Đổi mật khẩu'=>'Change password',
+    'Thoát hệ thống'=>'Log out',
+    'Chọn'=>'Select',
+    'Ngôn ngữ'=>'Language',
+    'Thông tin tài khoản' => 'Account Information',
+    'Tin nhắn' => 'Message',
+    'Thông tin cá nhân' => 'Persional Information',
+    'Họ, tên đệm' => 'Last name',
+    'Tên' => 'First name',
+    'Giới tính' => 'Gender',
+    'Ngày sinh' => 'Birthday',
+    'Số điện thoại' => 'Tel',
+    'Địa chỉ' => 'Address',
+    'Vui lòng liên hệ khi cần thay đổi thông tin cá nhân' => 'Please contact us if you need to change your profile',
+    'Đổi mật khẩu' => 'Change Password',
+    'Địa chỉ Email' => 'Email',
+    'Mật khẩu cũ' => 'Current Password',
+    
+    'Nhập mật khẩu cũ' => 'type current password',
+    'Mật khẩu mới' => 'New password',
+    
+    'Nhập mật khẩu mới' => 'type new password',
+    
+    'Nhập lại mật khẩu mới' => 'Confirm Password',
+    
+    'Đổi mật khẩu' => 'Change password',
+    'Lớp/môn học'=>'Class/Subject',
+    'Khóa học'=>'Course',
+    'Chủ đề buổi học'=>'Topic',
+    'Giáo viên'=>'Teacher',
+    'Ngày học'=>'Date',
+    'Thời gian học'=>'Time',
+    'Thoát hệ thống'=>'Exit',
+    'Gửi tin nhắn cho speakup.vn'=>'Send message to us',
+    'Tiêu đề tin nhắn'=>'Subject',
+    'Nội dung tin nhắn'=>'Message',
+    'Gửi tin nhắn'=>'Send',
+    'Hộp tin nhắn đến'=>'Inbox',
+    'Tin nhắn đã gửi'=>'Sent',
+    'Thông báo'=>'Notification',
+    'Bạn chưa có thông báo mới'=>'You don\'t have any new notifications',
+    'Bạn chưa có thêm tin nhắn mới'=>'You don\'t have any new messages',
+    'Thông báo từ tutor.vn'=>'Notification',
+    'Hiện tại không có thông báo mới.'=>'You do not have any new notifications',
+    
+    'Không có tin nhắn gửi đi'=>'You do not have any sent message',
+    'Không có tin nhắn đến'=>'You have no message',
+    'Kiểm tra loa, microphone'=>'Test Speaker, Microphone',
+    'Để tham gia chương trình học này yêu cầu bạn cần có các điều kiện sau'=>'Computer/laptop Pre-requisites',
+    'Máy tính có kết nối internet ADSL hoặc tốt hơn'=>' ADSL internet connection or better',
+    'Sử dụng trình duyệt Chrome, CờRôm+, hoặc Firefox'=>' Using Google Chrome or Mozilla Firefox browser ',
+    'Loa và Micro sử dụng tốt'=>' Microphone and Speaker works properly',
+    'Nên có webcam'=>' Webcam functions properly',
+    'Kiểm tra mic và loa (với trình duyệt'=>'Test Speaker and Microphone (using',
+    'Trình duyệt bạn đang sử dụng là'=>'You are using',
+    'Đạt yêu cầu để tham gia chương trình'=>'Your browser meets the requirements',
+    'Chú ý'=>'Node',
+    'Kiểm tra mic và loa'=>'Test Speaker and Microphone',
+    'phiên bản'=>' version',
+    'Kiểm tra trình duyệt'=>'Browser check',
+    'Bấm'=>'Click',
+    'ghi âm'=>'record',
+    'Ghi âm'=>'Record',
+    'cho phép'=>'Allow',
+    'nếu trình duyệt hỏi'=>' if the browser asks you to',
+    'Xem ảnh hướng dẫn ở dưới'=>'Please see the instruction below',
+    'Nói gần mic, rõ ràng'=>'Speak to the mic clearly',
+    'Nghe lại để kiểm tra loa'=>'Listen to the playback to make sure \n the speaker funtions properly',
+    'Sau 4 bước trên nếu bạn không gặp lỗi gì thì có thể tham gia chương trình của chúng tôi'=>
+    'After all tests are passed, you can start using our program',
+    'Nếu bạn không ghi âm hoặc nghe lại được vui lòng liên hệ với chúng tôi để được tư vấn cụ thể hơn'=>
+    'If the recording or playback did not goes well, you may contact us for more information',
+    'Bạn có thể gọi vào số hotline 0961.00.50.57 hoặc để lại lời nhắn gồm'=>'Hotline: 0961.00.50.57 or leave us a message',
+    'Tên và số điện thoại'=>'with your name and phone number',
+    'trong ô LiveChat "Cần hỗ trợ?" ở góc dưới bên phải, đội ngũ tư vấn của chúng tôi sẽ gọi điện hỗ trợ bạn'=>
+    'in the LiveChat pannel in the bottom right, our support team will contact you ASAP',
+    'Bạn đang dùng trình duyệt Firefox. Chúng tôi khuyên dùng Google Chrome để có thể tương thích tốt nhất với hệ thống'=>
+    'You are using Firefox browser. We recommend that you use Google Chrome for best compabilities. A download link can be found below',
+    'Tải Google Chrome'=>'Download Google Chrome',
+
+);
