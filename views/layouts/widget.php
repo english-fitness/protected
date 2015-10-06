7f295f194f4c6d48fc35b5a57298d654c567790f
thphuong <thp.1093@outlook.com>
2015-10-06 16:37:31 +0700

    layout for iframe in student unified management ui

---
+++ views/layouts/widget.php
@@ -0,0 +1,20 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->baseUrl; ?>/media/css/base/style.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/bootstrap/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/admin.css" />
+	<style type="text/css">
+		body{
+			padding: 0 0 0 0;
+		}
+		.grid-view{
+			padding: 0 0 0 0;
+		}
+	</style>
+</head>
+<body>
+<?php echo $content;?>
+</body>
+</html>
