8762e0d09872d8c01243f759e0291b87df142dd1
thphuong <thp.1093@outlook.com>
2015-10-20 10:07:52 +0700

    New login page appearance

--- views/site/index.php
+++ views/site/index.php
@@ -18,28 +18,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
   </head>
   <body>
-    <?php
-        $tenmien="http";
-        if(isset($_SERVER['HTTPS'])){
-            $tenmien.="s";
-        }
-        $tenmien.="://".$_SERVER['SERVER_NAME'];
-    ?>
-
     <div class="container">
         <div class="row">
-            <div class="col-sm-3 col-md-4 ">
-                <img class="trai-img" src="<?php echo Yii::app()->baseUrl; ?>/media/images/trai.png" alt="">
-            </div>
-            <div class="col-sm-6 col-md-4 ">
+            <div class="col-sm-6 col-md-4" style="margin: 0 auto; float:none; min-width:400px">
                 <div class="account-wall">
-		    <a href="<?php echo $tenmien?>">
-                        <img class="profile-img" src="<?php echo Yii::app()->baseUrl; ?>/media/images/logo-speakup.png" alt="">
-		    </a>
+                    <div style="text-align:center; width:100%">
+            		    <a href="/">
+                            <img style="width: 180px;" src="<?php echo Yii::app()->baseUrl; ?>/media/images/logo/logo-white-bordered-500.png" alt="">
+            		    </a>
+                    </div>
                     <form name="login-form" class="form-signin" action="<?php echo Yii::app()->baseurl;?>/site/signin" method="post">
                         
-                        <input name="username" type="text" class="form-control" placeholder="Username ..." required autofocus>
-                        <input name="password" type="password" class="form-control" placeholder="Password ..." required>
+                        <input name="username" type="text" class="form-control username" placeholder="Username ..." required autofocus>
+                        <input name="password" type="password" class="form-control password" placeholder="Password ..." required>
                         <span class="text-center test-error">
                             <?php 
                             foreach(Yii::app()->user->getFlashes() as $key => $message) : 
@@ -47,24 +38,18 @@
                             endforeach;
                             ?>
                         </span>
-                        <a href="<?php echo $tenmien."/news/dang-ky" ?>" class="pull-left new-account">Create an account? </a> 
-                        <a href='<?php echo $tenmien."/news/lien-he"?>' class="pull-right need-help">Need help? </a>
-                        <span class="clearfix"></span>
                         <button class="btn btn-lg btn-warning btn-block btn-login" type="submit">
-                            Sign in</button>
+                            Sign in
+                        </button>
+                        <a href="/news/dang-ky" class="pull-left new-account">Create an account? </a> 
+                        <a href='/news/lien-he' class="pull-right need-help">Need help? </a>
+                        <span class="clearfix"></span>
                     </form>
                 </div>
-                
-            </div>
-            <div class="col-sm-3 col-md-4 ">
-                <img class="phai-img" src="<?php echo Yii::app()->baseUrl; ?>/media/images/phai.png" alt="">
             </div>
         </div>
     </div>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="<?php echo Yii::app()->baseUrl; ?>/media/js/bootstrap/jquery.min.js"></script>
-      
     <!-- Latest compiled and minified JavaScript -->
     <script src="<?php echo Yii::app()->baseUrl; ?>/media/js/bootstrap/bootstrap.min.js"></script>
 
